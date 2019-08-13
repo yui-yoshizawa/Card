7df54f0e0b6fc177780400b31e1bf39705b80bda
yui-yoshizawa <yui.yossy.0114@gmail.com>
2019-08-13 20:52:46 +0900

    配列を修正

--- Card/ViewController.swift
+++ Card/ViewController.swift
@@ -30,8 +30,9 @@ class ViewController: UIViewController {
     // ユーザーリスト
     let nameList: [[String: String]] = [
         ["personImage": "津田梅子", "personName": "津田梅子", "personProfession": "教師", "personHometown": "千葉"],
-        ["personImage": "ジョージワシントン", "personName": "ジョージワシントン", "personProfession": "大統領", "personHometown": "アメリカ"],
         ["personImage": "ガリレオガリレイ", "personName": "ガリレオガリレイ", "personProfession": "物理学者", "personHometown": "イタリア"],
+        ["personImage": "ジョージワシントン", "personName": "ジョージワシントン", "personProfession": "大統領", "personHometown": "アメリカ"],
+        
         ["personImage": "板垣退助", "personName": "板垣退助", "personProfession": "議員", "personHometown": "高知"],
         ["personImage": "ジョン万次郎", "personName": "ジョン万次郎", "personProfession": "冒険家", "personHometown": "アメリカ"]
     ]
